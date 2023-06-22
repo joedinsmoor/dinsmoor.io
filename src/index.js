b38ec7e032935ba512e861a45ceec1d775c65d71
Joe Dinsmoor <jdinsmoor@dinsmoor.pw>
2023-06-21 20:18:53 -0400

    modified underlying architecture

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom";
-class Welcome extends React.Component {
-  render() {
-    return <h1>Hello World from React boilerplate</h1>;
-  }
-}
-ReactDOM.render(<Welcome />, document.getElementById("root"));
+console.log("Hello World!");
